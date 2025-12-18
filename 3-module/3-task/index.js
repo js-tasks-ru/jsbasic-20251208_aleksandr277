a1fdec5b82cf27092e7b337a3ebf730799449b7a
Merenov Aleksandr <aleksandr.merenov@auxo.team>
2025-12-18 21:54:32 +0300

    add 3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  const arr = str.split('-')
+  let result = ''
+  arr.forEach((word, index) => index === 0 ?
+    result += word :
+    result += word[0].toUpperCase() + word.slice(1))
+  return result
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,4 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str.split(' ').filter(item => !isNaN(item))
+  return { min: Math.min(...arr), max: Math.max(...arr) }
 }
