22dee3ffd3c724397228c8c91f266cafd410861a
Merenov Aleksandr <aleksandr.merenov@auxo.team>
2026-01-21 22:53:08 +0300

    add 7-module

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,54 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = this.render()
+    this.onEscKeyDown = this.onEscKeyDown.bind(this)
+  }
+
+  render() {
+    return createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button class="modal__close">
+              <img src="../../assets/images/icons/cross-icon.svg">
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `)
+  }
+
+  open() {
+    document.body.append(this.elem)
+    document.body.classList.add('is-modal-open')
+
+    this.elem.querySelector('.modal__close').addEventListener('click', () => this.close())
+
+    document.addEventListener('keydown', this.onEscKeyDown)
+  }
+
+  close() {
+    this.elem.remove()
+    document.body.classList.remove('is-modal-open')
+
+    document.removeEventListener('keydown', this.onEscKeyDown)
+  }
+
+  setTitle(title) {
+    this.elem.querySelector('.modal__title').textContent = title
+  }
+
+  setBody(node) {
+    const body = this.elem.querySelector('.modal__body')
+    body.innerHTML = ''
+    body.append(node)
+  }
+
+  onEscKeyDown(event) {
+    if (event.code === 'Escape') this.close()
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,70 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps
+    this.value = value
+
+    this.elem = this.render()
+    this.updateSlider()
+
+    this.elem.addEventListener('click', this.onClick)
+  }
+
+  render() {
+    const stepsHtml = Array.from({ length: this.steps }, (_, index) => {
+      return `<span ${index === this.value ? 'class="slider__step-active"' : ''}></span>`
+    }).join('')
+
+    return createElement(`
+      <div class="slider">
+        <div class="slider__thumb">
+          <span class="slider__value">${this.value}</span>
+        </div>
+        <div class="slider__progress"></div>
+        <div class="slider__steps">
+          ${stepsHtml}
+        </div>
+      </div>
+    `)
+  }
+
+  onClick = (event) => {
+    const sliderRect = this.elem.getBoundingClientRect()
+    const left = event.clientX - sliderRect.left
+    const leftRelative = left / this.elem.offsetWidth
+
+    const segments = this.steps - 1
+    const approximateValue = leftRelative * segments
+    const value = Math.round(approximateValue)
+
+    this.value = value
+    this.updateSlider()
+
+    this.elem.dispatchEvent(
+      new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      })
+    )
+  }
+
+  updateSlider() {
+    const thumb = this.elem.querySelector('.slider__thumb')
+    const progress = this.elem.querySelector('.slider__progress')
+    const valueElem = this.elem.querySelector('.slider__value')
+    const steps = this.elem.querySelectorAll('.slider__steps span')
+
+    const segments = this.steps - 1
+    const valuePercents = this.value / segments * 100
+
+    thumb.style.left = `${valuePercents}%`
+    progress.style.width = `${valuePercents}%`
+
+    valueElem.textContent = this.value
+
+    steps.forEach((step, index) => {
+      step.classList.toggle('slider__step-active', index === this.value)
+    })
   }
 }
